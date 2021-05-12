419c71313887afa74237c9253c811a09506afbaa
bissbotch <64175657+bissbotch@users.noreply.github.com>
2021-05-12 02:31:25 +0100

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -1,16 +1,24 @@
 import discord
 from discord.ext import commands
-import dotenv
+from discord_slash import SlashCommand, SlashContext
+from dotenv import load_dotenv
 import os
+import json
 
+load_dotenv()
 TOKEN = os.getenv('TOKEN')
 PREFIX = os.getenv('PREFIX')
 
-bot = discord.Client()
+bot = commands.Bot(command_prefix=PREFIX)
+slash = SlashCommand(bot)
 
 @bot.event
 async def on_ready():
     print(f"We have logged in as {bot.user}")
     await bot.change_presence(activity=discord.CustomActivity(name =f"slaying it"))
+
+@slash.slash(name="hello")
+async def hello(ctx:SlashContext):
+    await ctx.hannel.send("Hello!")
 
 bot.run(TOKEN)
